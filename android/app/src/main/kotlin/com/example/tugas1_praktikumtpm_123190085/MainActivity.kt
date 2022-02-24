1b6ea224bb32a79d7766ff8867cd150673479f02
Daffa Nabil Libriana <ndaffanabil@gmail.com>
2022-02-24 12:05:34 +0700

    initial commit

---
+++ android/app/src/main/kotlin/com/example/tugas1_praktikumtpm_123190085/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tugas1_praktikumtpm_123190085
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: app_interface(),
+    );
+  }
+
+  Widget app_interface() {
+    return Scaffold(
+      appBar: AppBar(title: Text("Login Screen")),
+      body: Center(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 150, 0, 50),
+              child: FlutterLogo(),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(100, 0, 100, 0),
+              child: TextField(
+                decoration: (InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(100.0),
+                  ),
+                  hintText: "Email",
+                  contentPadding: EdgeInsets.all(20.0),
+                )),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(100, 15, 100, 0),
+              child: TextField(
+                decoration: (InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(100.0),
+                  ),
+                  hintText: "Password",
+                  contentPadding: EdgeInsets.all(20.0),
+                )),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text("Log In"),
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                  minimumSize: const Size(300, 40),
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {},
+              child: Text("Forgot password ?"),
+              style: TextButton.styleFrom(
+                primary: Colors.grey,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
