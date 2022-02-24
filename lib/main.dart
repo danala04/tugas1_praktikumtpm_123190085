5a3c528b8bc2d6ab5f9b6d7d51dc18e18066cbcd
Daffa Nabil Libriana <ndaffanabil@gmail.com>
2022-02-24 12:22:47 +0700

    Change Color 2

--- lib/main.dart
+++ lib/main.dart
@@ -55,7 +55,7 @@ class MyApp extends StatelessWidget {
                 onPressed: () {},
                 child: Text("Log In"),
                 style: ElevatedButton.styleFrom(
-                  primary: Colors.yellow,
+                  primary: Colors.blue,
                   minimumSize: const Size(300, 40),
                 ),
               ),
